13258e5d6d9117ecb66688c086de6f08e3ceb85a
HamzahCR7 <aslamhamzah15@gmail.com>
2020-08-02 12:52:55 +0530

    up

---
+++ index.js
@@ -0,0 +1,19 @@
+document
+    .getElementById('card')
+    .addEventListener('click', 
+        function() {
+          this
+              .classList
+              .toggle('friend-card__open');
+      }
+	);
+
+
+
+
+
+alsolike(
+    "RaeEEz", "More Gooey Stuff", 
+    "zvVdgr", "Isometric Social Buttons", 
+    "gaZwVr", "TicTacToe Game"
+)
